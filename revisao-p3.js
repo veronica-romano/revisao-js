124b3da85f186b6bee7dce802bc399f64dbf3a77
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-07 11:26:05 -0300

    inputs created

--- revisao-p3.js
+++ revisao-p3.js
@@ -1,3 +1,10 @@
+let nome = document.getElementById("nome").value;
+let nasc = document.getElementById("nasc").value;
+var data = new Date();
+var ano = data.getFullYear();
+let idade = ano - nasc;
+
+console.log(idade);
 
 
 
