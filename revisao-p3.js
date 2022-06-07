aecf8d65329114c635c1b3832336bad96a0d7bd9
Veronica Silva <veronica.lima.silva@uni9.edu.br>
2022-06-07 11:39:40 -0300

    form function

--- revisao-p3.js
+++ revisao-p3.js
@@ -1,10 +1,32 @@
-let nome = document.getElementById("nome").value;
-let nasc = document.getElementById("nasc").value;
-var data = new Date();
-var ano = data.getFullYear();
-let idade = ano - nasc;
 
-console.log(idade);
+let formulario = document.querySelector("form");
+formulario.addEventListener('submit', function(event){
+
+    event.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let nasc = document.getElementById("nasc").value;
+
+    var data = new Date();
+    var ano = data.getFullYear();
+    let idade = ano - nasc;
+
+    if (idade >= 18) {
+        conteudo.innerHTML = `${nome} tem ${idade} anos e já pode dirigir.`
+        if (idade > 30) {
+            conteudo.innerHTML = `${nome}, aconselhamos comprar um carro novo pois você tem ${idade} anos.`
+        } else{
+            conteudo.innerHTML = `${nome}, aconselhamos comprar um carro usado pois você tem ${idade} anos.`
+        }
+    } else{
+        conteudo.innerHTML = `${nome} tem ${idade} anos e NÃO pode dirigir.`
+    }
+
+})
+
+   
+
+
 
 
 
